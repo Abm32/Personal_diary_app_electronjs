c7df6eb50f984f5af0944a84a0e67ff3e5b2c76b
Abm32 <110992125+Abm32@users.noreply.github.com>
2023-05-06 20:09:18 +0530

    Initial commit

---
+++ main.js
@@ -0,0 +1,16 @@
+const { app, BrowserWindow } = require('electron');
+
+function createWindow () {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+  })
+
+  win.loadFile('src/index.html')
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit();
+})
